bcc5502d50e1cef24a2982a0505b046750d41f9e
JonatasAS <jonatas23997@live.com>
2024-01-18 23:28:33 -0300

    config extenções e plugins

--- Pizza-Shop/src/App.tsx
+++ Pizza-Shop/src/App.tsx
@@ -1,12 +1,15 @@
-import { Button } from "./components/ui/button";
 import './global.css'
 
-export function App() {
+import { Helmet, HelmetProvider } from 'react-helmet-async'
+import { RouterProvider } from 'react-router-dom'
 
+import { router } from './routes'
+
+export function App() {
   return (
-   <div className="flex justify-center items-center h-screen">
-    <Button>Enviar</Button>
-   </div>
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | Pizza Shop" />
+      <RouterProvider router={router} />
+    </HelmetProvider>
   )
 }
-
---
+++ Pizza-Shop/src/routes.tsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from 'react-router-dom'
+
+import { AppLayout } from './page/_layouts/app'
+import { AuthLayout } from './page/_layouts/auth'
+import { Dashboard } from './page/app/dashboard'
+import { SignIn } from './page/auth/sing-in'
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout />,
+    children: [{ path: '/', element: <Dashboard /> }],
+  },
+
+  {
+    path: '/',
+    element: <AuthLayout />,
+    children: [{ path: '/sign-in', element: <SignIn /> }],
+  },
+])
