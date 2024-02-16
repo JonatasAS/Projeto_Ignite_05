637684ea8073cedd502a1618a9b466c9643e498b
JonatasAS <jonatas23997@live.com>
2024-02-15 21:42:44 -0300

    criando conexão com api

--- Pizza-Shop/src/routes.tsx
+++ Pizza-Shop/src/routes.tsx
@@ -2,11 +2,11 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { AppLayout } from './page/_layouts/app'
 import { AuthLayout } from './page/_layouts/auth'
+import { NotFound } from './page/404'
 import { Dashboard } from './page/app/dashboard/dashboard'
 import { Orders } from './page/app/orders/orders'
 import { SignIn } from './page/auth/sign-in'
 import { SignUp } from './page/auth/sign-up'
-import { NotFound } from './page/404'
 
 export const router = createBrowserRouter([
   {
